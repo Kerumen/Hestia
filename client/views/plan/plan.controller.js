c7549211b15207c696d8b39f24ad8eafd098c158
Yann Pringault <ypringau@student.42.fr>
2015-03-19 13:26:37 +0100

    feat(staff): add productivity

--- client/views/plan/plan.controller.js
+++ client/views/plan/plan.controller.js
@@ -17,7 +17,8 @@ angular.module('app')
             housemaid: null,
             specificity: {
               vip: true,
-              typeOfLeave: 'départ'
+              typeOfLeave: 'départ',
+              type: 'single'
             }
           },
           {
@@ -27,7 +28,8 @@ angular.module('app')
             housemaid: null,
             specificity: {
               vip: true,
-              typeOfLeave: 'départ'
+              typeOfLeave: 'départ',
+              type: 'twin'
             }
           },
           {
@@ -37,7 +39,8 @@ angular.module('app')
             housemaid: null,
             specificity: {
               vip: true,
-              typeOfLeave: 'départ'
+              typeOfLeave: 'départ',
+              type: 'single'
             }
           },
           {
@@ -47,7 +50,8 @@ angular.module('app')
             housemaid: null,
             specificity: {
               vip: true,
-              typeOfLeave: 'départ'
+              typeOfLeave: 'départ',
+              type: 'twin'
             }
           },
           {
@@ -56,13 +60,10 @@ angular.module('app')
             occupied: false,
             specificity: {
               vip: true,
-              typeOfLeave: 'recouche'
+              typeOfLeave: 'recouche',
+              type: 'double'
             },
-            housemaid: {
-              name: 'Agathe',
-              picture: 'http://api.randomuser.me/portraits/thumb/women/2.jpg',
-              timeElapsed: 255787
-            }
+            housemaid: null
           },
           {
             number: 105,
@@ -71,7 +72,8 @@ angular.module('app')
             housemaid: null,
             specificity: {
               vip: true,
-              typeOfLeave: 'départ'
+              typeOfLeave: 'départ',
+              type: 'double'
             }
           },
           {
@@ -81,7 +83,8 @@ angular.module('app')
             housemaid: null,
             specificity: {
               vip: true,
-              typeOfLeave: 'départ'
+              typeOfLeave: 'départ',
+              type: 'single'
             }
           },
           {
@@ -91,7 +94,8 @@ angular.module('app')
             housemaid: null,
             specificity: {
               vip: true,
-              typeOfLeave: 'départ'
+              typeOfLeave: 'départ',
+              type: 'double'
             }
           },
           {
@@ -105,7 +109,8 @@ angular.module('app')
             },
             specificity: {
               vip: true,
-              typeOfLeave: 'recouche'
+              typeOfLeave: 'recouche',
+              type: 'twin'
             }
           },
           {
@@ -115,7 +120,8 @@ angular.module('app')
             housemaid: null,
             specificity: {
               vip: true,
-              typeOfLeave: 'départ'
+              typeOfLeave: 'départ',
+              type: 'twin'
             }
           },
           {
@@ -125,7 +131,8 @@ angular.module('app')
             housemaid: null,
             specificity: {
               vip: true,
-              typeOfLeave: 'recouche'
+              typeOfLeave: 'recouche',
+              type: 'double'
             }
           }
         ]
@@ -135,68 +142,56 @@ angular.module('app')
         name: 'Étage 2',
         rooms: [
           {
-            number: 100,
+            number: 200,
             state: 'available',
             occupied: true,
             housemaid: null,
             specificity: null
           },
           {
-            number: 101,
+            number: 201,
             state: 'unavailable',
             occupied: false,
             housemaid: null,
             specificity: null
           },
           {
-            number: 102,
+            number: 202,
             state: 'dirty',
             occupied: true,
             housemaid: null,
             specificity: null
           },
           {
-            number: 103,
+            number: 203,
             state: 'available',
             occupied: true,
             housemaid: null,
             specificity: null
           },
           {
-            number: 103,
-            state: 'dirty',
-            occupied: false,
-            specificity: {
-              vip: false
-            },
-            housemaid: {
-              name: 'Agathe',
-              timeElapsed: 255787
-            }
-          },
-          {
-            number: 104,
+            number: 204,
             state: 'available',
             occupied: false,
             housemaid: null,
             specificity: null
           },
           {
-            number: 105,
+            number: 205,
             state: 'cleaned',
             occupied: false,
             housemaid: null,
             specificity: null
           },
           {
-            number: 106,
+            number: 206,
             state: 'available',
             occupied: false,
             housemaid: null,
             specificity: null
           },
           {
-            number: 107,
+            number: 207,
             state: 'dirty',
             occupied: false,
             housemaid: {
@@ -206,7 +201,7 @@ angular.module('app')
             specificity: null
           },
           {
-            number: 108,
+            number: 208,
             state: 'dirty',
             occupied: false,
             housemaid: null,
@@ -215,14 +210,14 @@ angular.module('app')
             }
           },
           {
-            number: 109,
+            number: 209,
             state: 'cleaned',
             occupied: true,
             housemaid: null,
             specificity: null
           },
           {
-            number: 110,
+            number: 210,
             state: 'available',
             occupied: false,
             housemaid: null,
@@ -235,68 +230,56 @@ angular.module('app')
         name: 'Étage 3',
         rooms: [
           {
-            number: 100,
+            number: 300,
             state: 'available',
             occupied: true,
             housemaid: null,
             specificity: null
           },
           {
-            number: 101,
+            number: 301,
             state: 'unavailable',
             occupied: false,
             housemaid: null,
             specificity: null
           },
           {
-            number: 102,
+            number: 302,
             state: 'dirty',
             occupied: true,
             housemaid: null,
             specificity: null
           },
           {
-            number: 103,
+            number: 303,
             state: 'available',
             occupied: true,
             housemaid: null,
             specificity: null
           },
           {
-            number: 103,
-            state: 'dirty',
-            occupied: false,
-            specificity: {
-              vip: false
-            },
-            housemaid: {
-              name: 'Agathe',
-              timeElapsed: 255787
-            }
-          },
-          {
-            number: 104,
+            number: 304,
             state: 'available',
             occupied: false,
             housemaid: null,
             specificity: null
           },
           {
-            number: 105,
+            number: 305,
             state: 'cleaned',
             occupied: false,
             housemaid: null,
             specificity: null
           },
           {
-            number: 106,
+            number: 306,
             state: 'available',
             occupied: false,
             housemaid: null,
             specificity: null
           },
           {
-            number: 107,
+            number: 307,
             state: 'dirty',
             occupied: false,
             housemaid: {
@@ -306,7 +289,7 @@ angular.module('app')
             specificity: null
           },
           {
-            number: 108,
+            number: 308,
             state: 'dirty',
             occupied: false,
             housemaid: null,
@@ -315,14 +298,14 @@ angular.module('app')
             }
           },
           {
-            number: 109,
+            number: 309,
             state: 'cleaned',
             occupied: true,
             housemaid: null,
             specificity: null
           },
           {
-            number: 110,
+            number: 310,
             state: 'available',
             occupied: false,
             housemaid: null,
@@ -425,32 +408,95 @@ angular.module('app')
       {
         name: 'Lisa',
         picture: '',
-        rooms : [101, 102, 103, 104]
+        rooms : [100, 101, 102, 103],
+        data: [
+          {
+            values: [{ x: 1, y: 12}, { x: 2, y: 20 }, { x: 3, y: 10 }, { x: 4, y: 21 }, { x: 5, y: 14 }, { x: 6, y: 15 }],
+            key: 'Productivité'
+          }
+        ]
       },
       {
         name: 'Zoé',
         picture: '',
-        rooms : [101, 102, 103, 104]
+        rooms : [200, 203, 207],
+        data: [
+          {
+            values: [{ x: 1, y: 28}, { x: 2, y: 23 }, { x: 3, y: 15 }, { x: 4, y: 24 }, { x: 5, y: 20 }, { x: 6, y: 20 }],
+            key: 'Productivité'
+          }
+        ]
       },
       {
-        name: 'Agathe',
+        name: 'Carole',
         picture: '',
-        rooms : [101, 102, 103, 104]
+        rooms : [305, 306, 302],
+        data: [
+          {
+            values: [{ x: 1, y: 17}, { x: 2, y: 12 }, { x: 3, y: 18 }, { x: 4, y: 17 }, { x: 5, y: 14 }, { x: 6, y: 23 }],
+            key: 'Productivité'
+          }
+        ]
       },
       {
         name: 'Fatima',
         picture: '',
-        rooms : [101, 102, 103, 104]
+        rooms : [204, 208, 104],
+        data: [
+          {
+            values: [{ x: 1, y: 28}, { x: 2, y: 23 }, { x: 3, y: 15 }, { x: 4, y: 24 }, { x: 5, y: 20 }, { x: 6, y: 20 }],
+            key: 'Productivité'
+          }
+        ]
       },
       {
         name: 'Franck',
         picture: '',
-        rooms : [101, 102, 103, 104]
+        rooms : [106, 104],
+        data: [
+          {
+            values: [{ x: 1, y: 15}, { x: 2, y: 12 }, { x: 3, y: 10 }, { x: 4, y: 12 }, { x: 5, y: 14 }, { x: 6, y: 15 }],
+            key: 'Productivité'
+          }
+        ]
       },
       {
         name: 'Rajid',
         picture: '',
-        rooms : [101, 102, 103, 104]
+        rooms : [109, 102, 108, 103],
+        data: [
+          {
+            values: [{ x: 1, y: 15}, { x: 2, y: 12 }, { x: 3, y: 10 }, { x: 4, y: 12 }, { x: 5, y: 14 }, { x: 6, y: 15 }],
+            key: 'Productivité'
+          }
+        ]
       }
-    ]
+    ];
+
+    vm.addRoom = function (index) {
+      vm.housemaids[index].rooms.push(vm.housemaids[index].add);
+      vm.housemaids[index].add = null;
+    };
+
+    vm.housemaidSelected = null;
+
+    vm.options = {
+      chart: {
+        type: 'lineChart',
+        height: 300,
+        x: function (d){ return d.x; },
+        y: function (d){ return d.y; },
+        interpolate: 'monotone',
+        forceY: [0, 30]
+      }
+    };
+
+    vm.startClean = function () {
+      vm.floors[0].rooms[4].housemaid = {
+        name: 'Carole',
+        picture: 'http://api.randomuser.me/portraits/thumb/women/55.jpg',
+        timeElapsed: 0
+      }
+    }
+
   });
